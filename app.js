7a1a2235e39995f782d1436cbb27a0773eae4a75
Hannah246 <44749816+Hannah246@users.noreply.github.com>
2019-02-02 21:01:28 -0500

    Geolocation

--- app.js
+++ app.js
@@ -31,7 +31,32 @@ function initMap() {
       zoom: 16,
       center: myLocation
     });
-    
+
+    //Geolocation things
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+        map.setCenter(pos);
+    });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+
+    //End of geolocation things
   
     function update() {
       
