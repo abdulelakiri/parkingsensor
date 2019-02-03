e5d3a24137ed6bd73c1166db734e4b11dfda97c1
AdamCooke00 <46229677+AdamCooke00@users.noreply.github.com>
2019-02-03 04:48:42 -0500

    last push from adam

--- cssScript.js
+++ cssScript.js
@@ -1,8 +1,24 @@
-window.addEventListener("scroll", function() {
-    if (window.scrollY > 1) {
+window.addEventListener("wheel", function(e) {
+    if(e.deltaY < 30){
+        $('.navbar').fadeIn(250);
+    }
+    if(e.deltaY > 0 ){
         $('.navbar').fadeOut(250);
     }
-    else {
-        $('.navbar').show();
-    }
 },false);
+
+$(".parkNow").on("click", function(){
+    console.log("Clicked!");
+    $(body).scrollTop(300);
+})
+
+$(".pageJump").click(function(){
+    var id = "#" + $(this).attr('goto');
+    var top = $(id).position().top;
+    $('html').scrollTop(top);
+  });
+
+$(".navDisable").click(function(){
+    console.log("HIDE!");
+    $('.navbar').toggle();
+})
